1106eb5cc9319e37e5e69d8d54eb48c0b0eb1884
shnepal121 <shnepal@gmail.com>
2020-12-05 21:57:47 +0545

    feat(app): add api and use mvc for binding ui and api

---
+++ Controllers/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIproject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIproject.Controllers
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<usr_login> Usr_login { get; set; }
+        public DbSet<saveSearch> saveSearch { get; set; }
+
+
+    }
+}
---
+++ Models/usr_login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIproject.Models
+{
+    public class usr_login
+    {
+        internal object user;
+        internal object password;
+
+        [Key]
+        public string usr_id { get; set; }
+        public string pass { get; set; }
+        public string email { get; set; }
+        public string tel { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string education { get; set; }
+    }
+}
